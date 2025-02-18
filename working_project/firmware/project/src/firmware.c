8747e5ef0c76de869416f5257b9c2f031c6869a9
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-18 11:43:31 +0100

    next step =)

--- working_project/firmware/project/src/firmware.c
+++ working_project/firmware/project/src/firmware.c
@@ -6,5 +6,19 @@ int main(void) {
 	print_str("hello world\n");
 	unsigned int test = 7;
 	print_dec(test);
-
+	unsigned int test2 = 6;
+	print_dec(test2);
+	unsigned int test3 = 5;
+	print_dec(test3);
+	unsigned int test4 = 4;
+	print_dec(test4);
+	unsigned int test5 = 3;
+	print_dec(test5);
+	unsigned int test6 = 2;
+	print_dec(test6);
+	unsigned int test7 = 1;
+	print_dec(test7);
+	unsigned int test8 = 0;
+	print_dec(test8);
+	
 }
