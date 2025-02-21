8c1583451a07614d8f7a3a8886adf4218e6a9f92
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-21 14:58:37 +0100

    push to roger

--- working_project/firmware/project/src/firmware.c
+++ working_project/firmware/project/src/firmware.c
@@ -1,5 +1,13 @@
 #include "print.h"
-#include "ex102.h"
+//#include "ex102.h"
+
+int multiply(int a, int b) {
+    int a_new = a;
+    for (int i = 0; i < b - 1; i++) {
+        a_new = a_new + a;
+    }
+    return a_new;
+}
 
 int main(void) {
 
@@ -8,6 +16,6 @@ int main(void) {
 	print_dec(test);
 	unsigned int test1 = 69;
 	print_dec(test1);
-	//int twaalf = multiply(3, 4);
-	//print_dec(twaalf);
+	int twaalf = multiply(3, 4);
+	print_dec(twaalf);
 }
