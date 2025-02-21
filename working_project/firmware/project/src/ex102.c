292baee33da88aad66e2cc6255cd03ec5f52ec65
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-21 15:22:00 +0100

    push to roger

--- working_project/firmware/project/src/ex102.c
+++ working_project/firmware/project/src/ex102.c
@@ -1,9 +1,9 @@
 #include "ex102.h"
 
-int multiply(int a, int b) {
-    int a_new = a;
-    for (int i = 0; i < b - 1; i++) {
-        a_new = a_new + a;
-    }
-    return a_new;
-}
+// int multiply(int a, int b) {
+//     int a_new = a;
+//     for (int i = 0; i < b - 1; i++) {
+//         a_new = a_new + a;
+//     }
+//     return a_new;
+// }
