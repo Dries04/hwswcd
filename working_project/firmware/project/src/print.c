04d01ddd5dafd6a0376cace59bba482ae7883f1a
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-21 22:18:22 +0100

    push to roger

--- working_project/firmware/project/src/print.c
+++ working_project/firmware/project/src/print.c
@@ -38,8 +38,8 @@ void print_dec(unsigned int val) {
 		
 		//new implementation for all values
 		while (val >= 10){
-			quotient = devide(val, devisor);
-			val = val - multiply(quotient, devisor);
+			quotient = devide(val, 10);
+			val = val - multiply(quotient, 10);
 			print_chr('0' + quotient);
 			if (val < 10){
 				print_chr('0' + val);
