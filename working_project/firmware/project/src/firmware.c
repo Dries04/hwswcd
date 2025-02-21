6a7aabccace908645e226652de14627155d54fb6
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-21 13:37:27 +0100

    push to roger

--- working_project/firmware/project/src/firmware.c
+++ working_project/firmware/project/src/firmware.c
@@ -4,15 +4,15 @@
 int main(void) {
 
 	print_str("hello world\n");
-	unsigned int test = 12;
-	print_dec(test);
-	unsigned int test2 = 6;
-	print_dec(test2);
-	unsigned int test3 = 5;
-	print_dec(test3);
-	print_str("\n");
-	unsigned int test9 = 69;
-	print_dec(test9);
-	unsigned int test10 = 420;
-	print_dec(test10);
+	// unsigned int test = 12;
+	// print_dec(test);
+	// unsigned int test2 = 6;
+	// print_dec(test2);
+	// unsigned int test3 = 5;
+	// print_dec(test3);
+	// print_str("\n");
+	// unsigned int test9 = 69;
+	// print_dec(test9);
+	// unsigned int test10 = 420;
+	// print_dec(test10);
 }
