0e49b03ded967767d9dc65087d51f63b2d4294ce
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-23 14:12:01 +0100

    push to roger

--- working_project/firmware/project/src/print.c
+++ working_project/firmware/project/src/print.c
@@ -21,87 +21,87 @@ void print_str(const char *p) {
 
 void print_dec(unsigned int val) {
 
-	if (val >= 10){
-		unsigned int quotient = 0;
-		int length_int = 1;
-		int devisor = 1;
+	// if (val >= 10){
+	// 	unsigned int quotient = 0;
+	// 	int length_int = 1;
+	// 	int devisor = 1;
 	
-		for (int i = 10; val >= multiply(i, 10); i = multiply(i, 10)){
-			length_int++;
-		}
-		while (length_int >= 0){
-			int temp = multiply(10, devisor);
-			devisor = temp;
-			length_int--;
-		}
+	// 	for (int i = 10; val >= multiply(i, 10); i = multiply(i, 10)){
+	// 		length_int++;
+	// 	}
+	// 	while (length_int >= 0){
+	// 		int temp = multiply(10, devisor);
+	// 		devisor = temp;
+	// 		length_int--;
+	// 	}
 
-		int devisor_temp = devisor;
-		int val_temp = val;
+	// 	int devisor_temp = devisor;
+	// 	int val_temp = val;
 
-		while (val_temp >= 10){
+	// 	while (val_temp >= 10){
 			
 			
-			//quotient = devide(val, devisor);
+	// 		//quotient = devide(val, devisor);
 			
-			int temp = 1;
-			int quotient1 = 0;
-			int a = val_temp;
-			int b = devisor_temp;
-			int d = devisor_temp;
+	// 		int temp = 1;
+	// 		int quotient1 = 0;
+	// 		int a = val_temp;
+	// 		int b = devisor_temp;
+	// 		int d = devisor_temp;
 
-			while (b <= a) {
-				b <<= 1;
-				temp <<= 1;
-			}
+	// 		while (b <= a) {
+	// 			b <<= 1;
+	// 			temp <<= 1;
+	// 		}
 		
-			while (temp > 1) {
-				b >>= 1;
-				temp >>= 1;
+	// 		while (temp > 1) {
+	// 			b >>= 1;
+	// 			temp >>= 1;
 		
-				if (a >= b) {
-					a -= b;
-					quotient1 += temp;
-				}
-			}
+	// 			if (a >= b) {
+	// 				a -= b;
+	// 				quotient1 += temp;
+	// 			}
+	// 		}
 
-			int c = quotient1;
-			//val = val - multiply(quotient, devisor);
+	// 		int c = quotient1;
+	// 		//val = val - multiply(quotient, devisor);
 
-			int a_new = c;
-			for (int i = 0; i < d - 1; i++) {
-				a_new = a_new + c;
-			}
-			val_temp = val_temp - a_new;
+	// 		int a_new = c;
+	// 		for (int i = 0; i < d - 1; i++) {
+	// 			a_new = a_new + c;
+	// 		}
+	// 		val_temp = val_temp - a_new;
 			
-			//devisor_temp = devisor_temp / 10;
+	// 		//devisor_temp = devisor_temp / 10;
 
-			int temp2 = 1;
-			int quotient2 = 0;
-			int hihi = 10;
+	// 		int temp2 = 1;
+	// 		int quotient2 = 0;
+	// 		int hihi = 10;
 		
-			while (hihi <= a) {
-				hihi <<= 1;
-				temp2 <<= 1;
-			}
+	// 		while (hihi <= a) {
+	// 			hihi <<= 1;
+	// 			temp2 <<= 1;
+	// 		}
 		
-			while (temp2 > 1) {
-				hihi >>= 1;
-				temp2 >>= 1;
+	// 		while (temp2 > 1) {
+	// 			hihi >>= 1;
+	// 			temp2 >>= 1;
 		
-				if (devisor_temp >= 10) {
-					devisor_temp -= 10;
-					quotient2 += temp2;
-				}
-			}
+	// 			if (devisor_temp >= 10) {
+	// 				devisor_temp -= 10;
+	// 				quotient2 += temp2;
+	// 			}
+	// 		}
 
-			devisor_temp = quotient2;
+	// 		devisor_temp = quotient2;
 
-			print_chr('0' + quotient1);
-			if (val_temp < 10){
-				print_chr('0' + val_temp);
-				print_str("\n");
-			}
-		}
+	// 		print_chr('0' + quotient1);
+	// 		if (val_temp < 10){
+	// 			print_chr('0' + val_temp);
+	// 			print_str("\n");
+	// 		}
+	// 	}
 
 		// while (val >= 10){
 		// 	quotient = devide(val, 10);
@@ -123,13 +123,49 @@ void print_dec(unsigned int val) {
 		// print_chr('0' + quotient);
 		// print_chr('0' + val);
 		// print_str("\n");
-	}else if (val < 10 ){
-		print_chr('0' + val);
-		print_str("\n");
-	}else if(val == 10){
-		print_chr('1');
-		print_chr('0');
-		print_str("\n");
+	// }else if (val < 10 ){
+	// 	print_chr('0' + val);
+	// 	print_str("\n");
+	// }else if(val == 10){
+	// 	print_chr('1');
+	// 	print_chr('0');
+	// 	print_str("\n");
+	// }
+
+	int num = val;
+    // Handle negative numbers
+    if (num < 0) {
+        print_char('-');
+        num = -num;
+    }
+
+	// Buffer to store digits (max 10 for 32-bit int, plus null terminator)
+	char buffer[12]; 
+	int i = 1;
+
+	// Extract digits using subtraction (since division is unavailable)
+	do {
+		int digit = 0;
+		int power = 1;
+
+		// Find the highest power of ten that fits into num
+		while (num >= power * 10 && power <= 1000000000) {
+			power = (power << 3) + (power << 1); // Equivalent to power *= 10
+		}
+
+		// Extract the most significant digit
+		while (num >= power) {
+			num -= power;
+			digit++;
+		}
+
+		// Store digit as character
+		buffer[i++] = '0' + digit;
+	} while (num > 0);
+
+	// Print the collected digits
+	for (int j = 1; j < i; j++) {
+		print_char(buffer[j]);
 	}
 }
 
