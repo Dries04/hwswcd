2d36ff87bc9ac8d168c79c7c1df7f384efa363e2
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-24 21:30:07 +0100

    push to roger

--- working_project/firmware/project/src/firmware.c
+++ working_project/firmware/project/src/firmware.c
@@ -3,10 +3,7 @@
 
 int main(void) {
 
-	print_str("hello world\n");
-	unsigned int test5 = 495;
-	print_dec(test5);
-	unsigned int test6 = convert(test5);
-	print_dec(test6);
+	unsigned int celcius = convert(593);
+	print_dec(celcius);
 
 }
