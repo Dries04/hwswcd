6da5fb57ed988268f544d5ac5fef159ed35bafe2
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-24 21:08:26 +0100

    push to roger

--- working_project/firmware/project/src/ex105.c
+++ working_project/firmware/project/src/ex105.c
@@ -1,23 +1,5 @@
 #include "ex105.h"
 
-int multiply(int a, int b) {
-    int result = 0;
-    while (b > 0) {
-        result += a;
-        b--;
-    }
-    return result;
-}
-
-// int divide(int a, int b) {
-//     int quotient = 0;
-//     while (a >= b) {
-//         a -= b;
-//         quotient++;
-//     }
-//     return quotient;
-// }
-
 unsigned int convert(unsigned int x) {
     int temp = x;
     
@@ -26,10 +8,15 @@ unsigned int convert(unsigned int x) {
     temp = x - sub;
 
     // Multiply by 5 (using addition)
-    temp = multiply(temp, 5);
+    int b = 5;
+    // int a = temp;
+    // while (b > 0) {
+    //     temp += a;
+    //     b--;
+    // }
 
     // Divide by 9 (using subtraction)
-    temp = divide(temp, 9);
+    // temp = divide(temp, 9);
 
     return temp;
 }
