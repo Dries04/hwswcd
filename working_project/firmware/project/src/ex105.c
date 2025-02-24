4b909dae44f6fb06fd0aabf9073f1a0176498dff
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-24 21:03:49 +0100

    push to roger

--- working_project/firmware/project/src/ex105.c
+++ working_project/firmware/project/src/ex105.c
@@ -1,10 +1,35 @@
 #include "ex105.h"
-#include "mathlib.h"
+
+int multiply(int a, int b) {
+    int result = 0;
+    while (b > 0) {
+        result += a;
+        b--;
+    }
+    return result;
+}
+
+int divide(int a, int b) {
+    int quotient = 0;
+    while (a >= b) {
+        a -= b;
+        quotient++;
+    }
+    return quotient;
+}
 
 unsigned int convert(unsigned int x) {
-    //convert farhenheit to celsius
-    int w = 0;
-    int z = x - 32;
-    w = multiply(7, 5);
-    return w;
+    int temp = x;
+    
+    // Subtract 32 (manually)
+    int sub = 32;
+    temp = x - sub;
+
+    // Multiply by 5 (using addition)
+    temp = multiply(temp, 5);
+
+    // Divide by 9 (using subtraction)
+    temp = divide(temp, 9);
+
+    return temp;
 }
