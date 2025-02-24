7dfd26645debfb6cdfc12f4b0bd8da16150f1d6c
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-24 20:54:38 +0100

    push to roger

--- working_project/firmware/project/src/ex105.c
+++ working_project/firmware/project/src/ex105.c
@@ -4,11 +4,6 @@
 unsigned int convert(unsigned int x) {
     //convert farhenheit to celsius
     int z = x - 32;
-    int w = devide(5, 9);
-    int a_new = z;
-    for (int i = 0; i < w - 1; i++) {
-        a_new = a_new + w;
-    }
-    return a_new;
-
+    int y = multiply(z, 5);
+    return devide(y, 9);
 }
