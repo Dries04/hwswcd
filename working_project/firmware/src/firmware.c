9894d3e44b2fe5054b098c9f8452999c0dd5ba60
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-02-24 22:10:59 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -3,10 +3,8 @@
 
 int main(void) {
 	
+	// aantal clockcycles = ...
 	unsigned int celcius = convert(593);
 	print_dec(celcius);
-	unsigned int okbeter = convert(593);
-	print_dec(okbeter);
-	unsigned int okbeter2 = convert(593);
-	print_dec(okbeter2);
+
 }
