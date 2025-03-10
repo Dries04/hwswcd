e9451426111f28bfe6566b4fcf7f6acf2cfd4436
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-10 17:28:45 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -6,15 +6,13 @@ int main(void) {
 	int led = 0;
 	// laat ledje elke seconde knipperen
 	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 400000000; j++) {
+		for (int j = 0; j < 40000000; j++) {
 			if (led == 0) {
-				led = 1;
-				*((volatile unsigned int*)0x80000000) = 0x01;
-			} else {
-				led = 0;
 				*((volatile unsigned int*)0x80000000) = 0x00;
+			} else {
+				*((volatile unsigned int*)0x80000000) = 0x01;
 			}
-			// zet ledje aan of uit
 		}
+		led = !led;
 	}
 }
