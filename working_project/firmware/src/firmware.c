82e96e88b8c90bcac703bea1a549ccd14aa0ac92
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-10 16:50:20 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -3,8 +3,18 @@
 
 int main(void) {
 	
-	// aantal clockcycles = 758
-	unsigned int celcius = convert(593);
-	print_dec(celcius);
-
+	int led = 0;
+	// laat ledje elke seconde knipperen
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 1000000; j++) {
+			if (led == 0) {
+				led = 1;
+				*((volatile unsigned int*)0x80000000) = 0x01;
+			} else {
+				led = 0;
+				*((volatile unsigned int*)0x80000000) = 0x00;
+			}
+			// zet ledje aan of uit
+		}
+	}
 }
