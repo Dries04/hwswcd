ac12d5cc2a4a6cdb570fd5ee9bae422295775493
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-10 17:38:00 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -1,18 +1,20 @@
 #include "print.h"
 #include "ex105.h"
+#define ADDR ((volatile unsigned int*)0x80000000)
 
-int main(void) {
-	
-	int led = 0;
-	// laat ledje elke seconde knipperen
-	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 40000000; j++) {
-			if (led == 0) {
-				*((volatile unsigned int*)0x80000000) = 0x00;
-			} else {
-				*((volatile unsigned int*)0x80000000) = 0x01;
-			}
-		}
-		led = !led;
-	}
+void delay() {
+    for (volatile unsigned int i = 0; i < 100000000; i++); // Simple delay loop
+}
+
+int main() {
+    unsigned int value = 0x01;
+    
+    while (1) {
+        *ADDR = value;
+        
+        value ^= 0x01; // Toggle between 0x01 and 0x00
+        delay(); // Wait for a while
+    }
+    
+    return 0;
 }
