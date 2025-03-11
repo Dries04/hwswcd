05c4d97694155c4a7d3a58550426a67963d12fa2
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-11 14:55:36 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -2,17 +2,20 @@
 #include "ex105.h"
 
 void delay() {
-    for (volatile unsigned int i = 0; i < 4000000; i++); // Simple delay loop
+    for (volatile unsigned int i = 0; i < 400000; i++); // Simple delay loop
 }
 
 int main() {
-    unsigned int value = 0x01;
     
+    int value = 0;
+
     while (1) {
         
-        *((volatile unsigned int*)0x80000000) = value; // Output the value to the LED  
+         
         value = 1; // Toggle between 0x01 and 0x00
+        *((volatile unsigned int*)0x80000000) = value; // Output the value to the LED 
         delay();
+
     }
     
     return 0;
