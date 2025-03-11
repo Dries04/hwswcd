6d5d8741dac96920e2170af8791036bd486c4528
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-11 11:30:12 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -1,16 +1,10 @@
 #include "print.h"
 #include "ex105.h"
-#define ADDR ((volatile unsigned int*)0x80000000)
-
-void delay() {
-    for (volatile unsigned int i = 0; i < 100000000; i++); // Simple delay loop
-}
 
 int main() {
-    unsigned int value = 0x01;
-    
-    while (1) {
-        *ADDR = value;
+
+    while(1){
+        *((volatile unsigned int*)0x80000000) = 0x01;
     }
     
     return 0;
