a4017e7a7dfefe8f85fe098a88562e78ef71a7cd
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-16 19:50:54 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -1,25 +1,39 @@
 #include "print.h"
 #include "ex105.h"
+#include "tcnt.h"
 
-void delay() {
-    for (volatile unsigned int i = 0; i < 4000000; i++); // Simple delay loop
+extern unsigned int sw_mult(unsigned int x, unsigned int y);
+
+struct matrix_t{
+	unsigned int a00;
+	unsigned int a10;
+	unsigned int a01;
+	unsigned int a11;
+};
+
+void matrix_mult(struct matrix_t * z, struct matrix_t * x, struct matrix_t * y) {
+	z->a00 = sw_mult(x->a00, y->a00) + sw_mult(x->a10, y->a01);
+	z->a10 = sw_mult(x->a00, y->a10) + sw_mult(x->a10, y->a11);
+	z->a01 = sw_mult(x->a01, y->a00) + sw_mult(x->a11, y->a01);
+	z->a11 = sw_mult(x->a01, y->a10) + sw_mult(x->a11, y->a11);
 }
 
 
 int main() {
-    
-    int value = 0;
 
-    while (1) {
+	unsigned int tcnt;
+	struct matrix_t m, n, o;
+	
+	m.a00 = 1; n.a00 = 5;
+	m.a10 = 2; n.a10 = 6;
+	m.a01 = 3; n.a01 = 7;
+	m.a11 = 4; n.a11 = 8;
 
-        for (int i = 0; i < 16; i++) {
-            value = value + 1;
-            *((volatile unsigned int*)0x80000000) = value;
-            delay();
-        }
+	TCNT_start();
+	matrix_mult(&o, &m, &n);
+	TCNT_stop();
+	tcnt = TCNT_VAL;
+	print_dec(tcnt);
 
-    }
-
-    
     return 0;
 }
