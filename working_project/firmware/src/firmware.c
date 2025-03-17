8b0a1ea85b7b930066aab1dda620e790005037be
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-17 09:16:00 +0100

    push to roger

--- working_project/firmware/src/firmware.c
+++ working_project/firmware/src/firmware.c
@@ -21,19 +21,23 @@ void matrix_mult(struct matrix_t * z, struct matrix_t * x, struct matrix_t * y) 
 
 int main() {
 
-	unsigned int tcnt;
-	struct matrix_t m, n, o;
-	
-	m.a00 = 1; n.a00 = 5;
-	m.a10 = 2; n.a10 = 6;
-	m.a01 = 3; n.a01 = 7;
-	m.a11 = 4; n.a11 = 8;
+	unsigned int result;
+	result = sw_mult(5, 6);
+	print_dec(result);
 
-	TCNT_start();
-	matrix_mult(&o, &m, &n);
-	TCNT_stop();
-	tcnt = TCNT_VAL;
-	print_dec(tcnt);
+	// unsigned int tcnt;
+	// struct matrix_t m, n, o;
+	
+	// m.a00 = 1; n.a00 = 5;
+	// m.a10 = 2; n.a10 = 6;
+	// m.a01 = 3; n.a01 = 7;
+	// m.a11 = 4; n.a11 = 8;
+
+	// TCNT_start();
+	// matrix_mult(&o, &m, &n);
+	// TCNT_stop();
+	// tcnt = TCNT_VAL;
+	// print_dec(tcnt);
 
     return 0;
 }
