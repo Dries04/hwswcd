2633f346dc50d67d9d483165cb8da25050e903d4
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-17 10:02:26 +0100

    push to roger

--- working_project/hdl/riscv_microcontroller.vhd
+++ working_project/hdl/riscv_microcontroller.vhd
@@ -61,6 +61,20 @@ architecture Behavioural of riscv_microcontroller is
             heartbeat : out STD_LOGIC
         );
     end component clock_and_reset_pynq;
+    
+    component wrapped_timer is
+        generic(
+            G_WIDTH : natural := 8
+        );
+        port(
+            clock : in STD_LOGIC;
+            reset : in STD_LOGIC;
+            iface_di : in STD_LOGIC_VECTOR(C_WIDTH-1 downto 0);
+            iface_a : in STD_LOGIC_VECTOR(C_WIDTH-1 downto 0);
+            iface_we : in STD_LOGIC;
+            iface_do : out STD_LOGIC_VECTOR(C_WIDTH-1 downto 0)
+        );
+    end component;
 
 
     -- (DE-)LOCALISING IN/OUTPUTS
@@ -86,6 +100,14 @@ architecture Behavioural of riscv_microcontroller is
     signal toid, toid_d : STD_LOGIC;
 
     signal leds : STD_LOGIC_VECTOR(6 downto 0);
+    
+    -- SIGNAL TO WRAPPER_TIMER
+    signal wr_tr_di, wr_tr_a, wr_tr_do: std_logic_vector(31 downto 0);
+    signal wr_tr_we: std_logic;
+    
+    -- SIGNALS FOR PERIPHERALS
+    signal peripheral_di, peripheral_a, peripheral_do: std_logic_vector (31 downto 0);
+    signal peripheral_we: std_logic;
 
 begin
 
@@ -99,6 +121,18 @@ begin
 
 
     gpio_leds_o <= leds(3 downto 0);
+    
+    -------------------------------------------------------------------------------
+    -- TIMER AND WRAPPER
+    -------------------------------------------------------------------------------
+    wrapped_timer_inst00: component wrapped_timer port map(
+        clock => clock,
+        reset => reset,
+        iface_di => wr_tr_di,
+        iface_a => wr_tr_a,
+        iface_we => wr_tr_we,
+        iface_do => wr_tr_do
+    );
 
     -------------------------------------------------------------------------------
     -- MICROPROCESSOR
@@ -107,32 +141,32 @@ begin
         clock => clock,
         reset => reset,
         ce => ce,
-        dmem_do => dmem_do,
-        dmem_we => dmem_we,
-        dmem_a => dmem_a,
-        dmem_di => dmem_di,
+        dmem_do => peripheral_do,
+        dmem_we => peripheral_we,
+        dmem_a => peripheral_a,
+        dmem_di => peripheral_di,
         instruction => instruction,
         PC => PC
     );
 
-PREG_CPU_CTRL: process(clock)
-    variable count : integer range 0 to 2 := 0;
-begin
-    if rising_edge(clock) then
-        if reset = '1' then 
-            count := 0;
-            ce <= '0';
-        else
-            if count = 2 then
+    PREG_CPU_CTRL: process(clock)
+        variable count : integer range 0 to 2 := 0;
+    begin
+        if rising_edge(clock) then
+            if reset = '1' then 
                 count := 0;
-                ce <= '1'; -- ce goes high on every third clock cycle
-            else
-                count := count + 1;
                 ce <= '0';
+            else
+                if count = 2 then
+                    count := 0;
+                    ce <= '1'; -- ce goes high on every third clock cycle
+                else
+                    count := count + 1;
+                    ce <= '0';
+                end if;
             end if;
         end if;
-    end if;
-end process;
+    end process;
 
     -------------------------------------------------------------------------------
     -- MEMORIES
@@ -172,6 +206,33 @@ end process;
                 if dmem_we = '1' and dmem_a = x"80000000" then 
                     leds <= dmem_di(6 downto 0);
                 end if;
+            end if;
+        end if;
+    end process;
+    
+    PERIPH: process (clock)
+    begin
+        if rising_edge(clock)then
+            if peripheral_a >= C_TIMER_BASE_ADDRESS_MASK then
+                wr_tr_we <= peripheral_we;
+                wr_tr_di <= peripheral_di;
+                wr_tr_do <= peripheral_do;
+                wr_tr_a <= peripheral_a;
+                
+                dmem_we <= '0';
+                dmem_di <= (others => '0');
+                dmem_do <= (others => '0');
+                dmem_a <= (others => '0'); 
+            else
+                dmem_we <= peripheral_we;
+                dmem_di <= peripheral_di;
+                dmem_do <= peripheral_do;
+                dmem_a <= peripheral_a; 
+                
+                wr_tr_we <= '0';
+                wr_tr_di <= (others => '0');
+                wr_tr_do <= (others => '0');
+                wr_tr_a <= (others => '0');
             end if;
         end if;
     end process;
