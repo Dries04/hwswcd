c49ede1d696243cc7ddf28f29a1e8847b1d3b998
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-03-20 15:57:42 +0100

    okok

--- working_project/firmware/course_example_1/src/firmware.c
+++ working_project/firmware/course_example_1/src/firmware.c
@@ -7,14 +7,19 @@
 
 #define LED              (*(volatile unsigned int *) LED_REG0_ADDRESS)
 
-
-
+int i = 1;
 
 
 void irq_handler(unsigned int cause) {
 
     if (cause & 4) {
         LED = 0xFFFFFFFF;
+    }
+    if(i < 8){
+        LED = i;
+        i++;
+    }else{
+        i = 1;
     }
 
     TCNT_CR = 0x17;
@@ -25,14 +30,12 @@ void irq_handler(unsigned int cause) {
 
 void main(void) {
     
-    unsigned int i=1, j;
+    //unsigned int i=1, j;
 
     TCNT_CMP = 0xC65D40;
     TCNT_start();
 
     while(1) {
-        for(i=0;i<8;i++) {
-            LED = i;
-        }
+
     }
 }
