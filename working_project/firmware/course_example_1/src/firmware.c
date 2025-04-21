5ac7b8805e8a15d3dfbda975177ae95f32831ccf
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-04-21 08:55:11 +0200

    print.h only

--- working_project/firmware/course_example_1/src/firmware.c
+++ working_project/firmware/course_example_1/src/firmware.c
@@ -1,5 +1,6 @@
 #include "print.h"
 #include "tcnt.h"
+#include <stdio.h>
 
 #define C_WIDTH 8
 #define C_HEIGHT 8
@@ -12,19 +13,18 @@
 #define QOI_OP_RGBA  0xFF
 
 void irq_handler(unsigned int cause) {
-
 }
 
 typedef struct {
     unsigned char r, g, b, a;
 } Pixel;
 
-int rgb_equal(Pixel a, Pixel b) {
-    return a.r == b.r && a.g == b.g && a.b == b.b;
+int rgb_equal(Pixel *a, Pixel *b) {
+    return a->r == b->r && a->g == b->g && a->b == b->b;
 }
 
-unsigned char pixel_hash(Pixel px) {
-    return (px.r * 3 + px.g * 5 + px.b * 7 + px.a * 11) % 64;
+unsigned char pixel_hash(Pixel *px) {
+    return (px->r * 3 + px->g * 5 + px->b * 7 + px->a * 11) % 64;
 }
 
 void write_byte(unsigned char **p, unsigned char byte) {
@@ -72,7 +72,7 @@ int main(void) {
     unsigned char b[C_HEIGHT][C_WIDTH];
     unsigned char a[C_HEIGHT][C_WIDTH];
 
-    Pixel index_array[64];  // just declare
+    Pixel index_array[64];
 
     for (int i = 0; i < 64; i++) {
         index_array[i].r = 0;
@@ -81,45 +81,46 @@ int main(void) {
         index_array[i].a = 0;
     }
 
+    Pixel prev;
+    prev.r = 0;
+    prev.g = 0;
+    prev.b = 0;
+    prev.a = 255;
 
-    Pixel prev = {0, 0, 0, 255};
     int run = 0;
 
-    unsigned char encoded[1024]; // enough for small image
+    unsigned char encoded[1024];
     unsigned char *p = encoded;
 
     initialise(r, g, b, a);
 
-    // Write QOI header
     write_byte(&p, 'q');
     write_byte(&p, 'o');
     write_byte(&p, 'i');
     write_byte(&p, 'f');
 
-    // Write 32-bit big-endian width
     write_byte(&p, (C_WIDTH >> 24) & 0xFF);
     write_byte(&p, (C_WIDTH >> 16) & 0xFF);
     write_byte(&p, (C_WIDTH >> 8) & 0xFF);
     write_byte(&p, C_WIDTH & 0xFF);
 
-    // Write 32-bit big-endian height
     write_byte(&p, (C_HEIGHT >> 24) & 0xFF);
     write_byte(&p, (C_HEIGHT >> 16) & 0xFF);
     write_byte(&p, (C_HEIGHT >> 8) & 0xFF);
     write_byte(&p, C_HEIGHT & 0xFF);
 
-    // Channels: 4 for RGBA, 3 for RGB
     write_byte(&p, 3);
-
-    // Colorspace: 0 = sRGB with linear alpha
     write_byte(&p, 0);
-
 
     for (unsigned char h = 0; h < C_HEIGHT; h++) {
         for (unsigned char w = 0; w < C_WIDTH; w++) {
-            Pixel px = { r[h][w], g[h][w], b[h][w], 255 };
+            Pixel px;
+            px.r = r[h][w];
+            px.g = g[h][w];
+            px.b = b[h][w];
+            px.a = 255;
 
-            if (rgb_equal(px, prev)) {
+            if (rgb_equal(&px, &prev)) {
                 run++;
                 if (run == 62) {
                     write_byte(&p, QOI_OP_RUN | (run - 1));
@@ -131,42 +132,33 @@ int main(void) {
                     run = 0;
                 }
 
-                unsigned char index_pos = pixel_hash(px);
-                if (rgb_equal(px, index_array[index_pos])) {
+                unsigned char index_pos = pixel_hash(&px);
+                if (rgb_equal(&px, &index_array[index_pos])) {
                     write_byte(&p, QOI_OP_INDEX | index_pos);
                 } else {
-                    index_array[index_pos] = px;
+                    index_array[index_pos].r = px.r;
+                    index_array[index_pos].g = px.g;
+                    index_array[index_pos].b = px.b;
+                    index_array[index_pos].a = px.a;
 
-                    int vr = (int)px.r;
-                    int vg = (int)px.g;
-                    int vb = (int)px.b;
+                    int dr = (int)px.r - (int)prev.r;
+                    int dg = (int)px.g - (int)prev.g;
+                    int db = (int)px.b - (int)prev.b;
 
-                    int pr = (int)prev.r;
-                    int pg = (int)prev.g;
-                    int pb = (int)prev.b;
+                    if (dr < -128) dr += 256;
+                    if (dg < -128) dg += 256;
+                    if (db < -128) db += 256;
 
-                    int dr = ((int)px.r - (int)prev.r + 256) % 256;
-                    if (dr > 127) dr -= 256;
-
-                    int dg = ((int)px.g - (int)prev.g + 256) % 256;
-                    if (dg > 127) dg -= 256;
-
-                    int db = ((int)px.b - (int)prev.b + 256) % 256;
-                    if (db > 127) db -= 256;
-
-
-                    // QOI_OP_DIFF
                     if ((dr >= -2 && dr <= 1) &&
                         (dg >= -2 && dg <= 1) &&
                         (db >= -2 && db <= 1)) {
-                        
+
                         unsigned char diff_byte = QOI_OP_DIFF |
                             ((dr + 2) << 4) |
                             ((dg + 2) << 2) |
                             (db + 2);
                         write_byte(&p, diff_byte);
                     }
-                    // QOI_OP_LUMA
                     else if ((dg >= -32 && dg <= 31)) {
                         int dr_dg = dr - dg;
                         int db_dg = db - dg;
@@ -180,15 +172,15 @@ int main(void) {
                         } else {
                             write_3(&p, px.r, px.g, px.b);
                         }
-                    }
-                    // Fallback: QOI_OP_RGB
-                    else {
+                    } else {
                         write_3(&p, px.r, px.g, px.b);
                     }
-
-
                 }
-                prev = px;
+
+                prev.r = px.r;
+                prev.g = px.g;
+                prev.b = px.b;
+                prev.a = px.a;
             }
         }
     }
@@ -197,18 +189,13 @@ int main(void) {
         write_byte(&p, QOI_OP_RUN | (run - 1));
     }
 
-    // End marker
     for (int i = 0; i < 7; i++) write_byte(&p, 0x00);
     write_byte(&p, 0x01);
 
-    // Output (for verification)
     for (unsigned char *i = encoded; i < p; i++) {
-        print_hex(*i, 1);
         //printf("%02X ", *i);
-        //OUTPORT = *i;
-        
+        print_hex(*i, 2);
     }
-    //print_str("\n");
 
     return 0;
 }
