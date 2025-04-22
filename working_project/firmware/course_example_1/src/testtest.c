15d1dbef44d201f0400b1a919c8f1e3987105039
Dries Ruttens <dries.ruttens@student.uhasselt.be>
2025-04-22 13:45:50 +0200

    test

--- working_project/firmware/course_example_1/src/testtest.c
+++ working_project/firmware/course_example_1/src/testtest.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "tcnt.h"
 
 #define C_WIDTH 8
@@ -9,10 +10,6 @@
 #define QOI_OP_RUN   0xC0 // 11xxxxxx
 #define QOI_OP_RGB   0xFE
 #define QOI_OP_RGBA  0xFF
-
-#define OUTPORT 0x80000000
-#define OUT_REG0_ADDRESS (OUTPORT + 0*4)
-#define LED (*(volatile unsigned int *) OUT_REG0_ADDRESS)
 
 extern unsigned int sw_mult(unsigned int x, unsigned int y);
 
@@ -39,10 +36,6 @@ void initialise(unsigned char r[C_HEIGHT][C_WIDTH], unsigned char g[C_HEIGHT][C_
     }
 }
 
-unsigned char pixel_hash(unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
-    return (sw_mult(r, 3) + sw_mult(g, 5) + sw_mult(b, 7) + sw_mult(a, 11)) & 0x3F;
-}
-
 int main(void) {
     unsigned char r[C_HEIGHT][C_WIDTH];
     unsigned char g[C_HEIGHT][C_WIDTH];
@@ -52,6 +45,7 @@ int main(void) {
     unsigned char r_prev = 0, g_prev = 0, b_prev = 0, a_prev = 255;
     int run = 0;
     unsigned int running_array[64];
+    int write_index = 0;
     int i;
 
     for (i = 0; i < 64; i++) {
@@ -60,11 +54,11 @@ int main(void) {
 
     initialise(r, g, b, a);
 
-    // Write header directly to LED
-    LED = 'q'; LED = 'o'; LED = 'i'; LED = 'f';
-    LED = 0x00; LED = 0x00; LED = 0x00; LED = C_WIDTH;
-    LED = 0x00; LED = 0x00; LED = 0x00; LED = C_HEIGHT;
-    LED = 0x03; LED = 0x00;
+    // Write QOI header
+    printf("%02X\n", 'q'); printf("%02X\n", 'o'); printf("%02X\n", 'i'); printf("%02X\n", 'f');
+    printf("%02X\n", 0x00); printf("%02X\n", 0x00); printf("%02X\n", 0x00); printf("%02X\n", C_WIDTH);
+    printf("%02X\n", 0x00); printf("%02X\n", 0x00); printf("%02X\n", 0x00); printf("%02X\n", C_HEIGHT);
+    printf("%02X\n", 0x03); printf("%02X\n", 0x00);
 
     for (i = 0; i < C_HEIGHT; i++) {
         for (int j = 0; j < C_WIDTH; j++) {
@@ -76,22 +70,33 @@ int main(void) {
             if (r_cur == r_prev && g_cur == g_prev && b_cur == b_prev && a_cur == a_prev) {
                 run++;
                 if (run == 62) {
-                    LED = (unsigned int) (QOI_OP_RUN | (run - 1));
+                    printf("%02X\n", QOI_OP_RUN | (run - 1));
                     run = 0;
                 }
             } else {
                 if (run > 0) {
-                    LED = (unsigned int) (QOI_OP_RUN | (run - 1));
+                    printf("%02X\n", QOI_OP_RUN | (run - 1));
                     run = 0;
                 }
 
-                unsigned char index = pixel_hash(r_cur, g_cur, b_cur, a_cur);
-                unsigned int current_pixel = ((unsigned int)r_cur << 24) | ((unsigned int)g_cur << 16) | ((unsigned int)b_cur << 8) | a_cur;
+                unsigned int current_pixel = ((unsigned int)r_cur << 24) |
+                                             ((unsigned int)g_cur << 16) |
+                                             ((unsigned int)b_cur << 8) |
+                                             a_cur;
 
-                if (running_array[index] == current_pixel) {
-                    LED = (unsigned int) (QOI_OP_INDEX | index);
+                int found_index = -1;
+                for (int k = 0; k < 64; k++) {
+                    if (running_array[k] == current_pixel) {
+                        found_index = k;
+                        break;
+                    }
+                }
+
+                if (found_index != -1) {
+                    printf("%02X\n", QOI_OP_INDEX | found_index);
                 } else {
-                    running_array[index] = current_pixel;
+                    running_array[write_index] = current_pixel;
+                    write_index = (write_index + 1) & 0x3F;
 
                     int dr = (int)r_cur - (int)r_prev;
                     int dg = (int)g_cur - (int)g_prev;
@@ -107,25 +112,25 @@ int main(void) {
                     else if (db > 127) db -= 256;
 
                     if ((dr >= -2 && dr <= 1) && (dg >= -2 && dg <= 1) && (db >= -2 && db <= 1)) {
-                        LED = (unsigned int) (QOI_OP_DIFF | ((dr + 2) << 4) | ((dg + 2) << 2) | (db + 2));
+                        printf("%02X\n", QOI_OP_DIFF | ((dr + 2) << 4) | ((dg + 2) << 2) | (db + 2));
                     } else if (dg >= -32 && dg <= 31) {
                         int dr_dg = dr - dg;
                         int db_dg = db - dg;
 
                         if ((dr_dg >= -8 && dr_dg <= 7) && (db_dg >= -8 && db_dg <= 7)) {
-                            LED = (unsigned int) (QOI_OP_LUMA | (dg + 32));
-                            LED = (unsigned int) ((dr_dg + 8) << 4) | (db_dg + 8);
+                            printf("%02X\n", QOI_OP_LUMA | (dg + 32));
+                            printf("%02X\n", ((dr_dg + 8) << 4) | (db_dg + 8));
                         } else {
-                            LED = (unsigned int) (QOI_OP_RGB);
-                            LED = (unsigned int) (r_cur);
-                            LED = (unsigned int) (g_cur);
-                            LED = (unsigned int) (b_cur);
+                            printf("%02X\n", QOI_OP_RGB);
+                            printf("%02X\n", r_cur);
+                            printf("%02X\n", g_cur);
+                            printf("%02X\n", b_cur);
                         }
                     } else {
-                        LED = (unsigned int) (QOI_OP_RGB);
-                        LED = (unsigned int) (r_cur);
-                        LED = (unsigned int) (g_cur);
-                        LED = (unsigned int) (b_cur);
+                        printf("%02X\n", QOI_OP_RGB);
+                        printf("%02X\n", r_cur);
+                        printf("%02X\n", g_cur);
+                        printf("%02X\n", b_cur);
                     }
                 }
 
@@ -137,14 +142,12 @@ int main(void) {
         }
     }
 
-    // Flush any remaining run
     if (run > 0) {
-        LED = (unsigned int) (QOI_OP_RUN | (run - 1));
+        printf("%02X\n", QOI_OP_RUN | (run - 1));
     }
 
-    // End marker: 7x 0x00, 1x 0x01
-    for (i = 0; i < 7; i++) LED = 0;
-    LED = 1;
+    for (i = 0; i < 7; i++) printf("%02X\n", 0x00);
+    printf("%02X\n", 0x01);
 
     return 0;
 }
